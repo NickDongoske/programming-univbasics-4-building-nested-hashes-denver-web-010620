36f05b2f305798fcbbbb7e80b2b843b5389a1f27
Nicholas Dongoske <nickdongoske@gmail.com>
2019-12-29 22:06:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,10 +17,10 @@ def monopoly_with_third_tier
   :railroads => {
     :pieces => 4,
     :names => {
-      :key,
-      :key,
-      :key,
-      :key
+      :key => 4 ,
+      :key => 4,
+      :key => 4,
+      :key => 4
     },
     :rent_in_dollars => {
       :one_piece_owned => 3,
